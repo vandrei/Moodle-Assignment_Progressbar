2877e178457ff15c51ff6e902e2a328c51d47545
= <=>
2013-04-10 00:04:05 +0300

    first commit

---
+++ lang/en/block_progressbar.php
@@ -0,0 +1,11 @@
+<?php
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['progressbar:addinstance'] = 'Add a progressbar block';
+$string['progressbar:myaddinstance'] = 'Add a progressbar block to my moodle';
+$string['pluginname'] = 'Assignment Progressbar';
+$string['noassgn'] = 'You have no pending assignments.';
+$string['nocourse'] = 'You are not enrolled in any courses.';
